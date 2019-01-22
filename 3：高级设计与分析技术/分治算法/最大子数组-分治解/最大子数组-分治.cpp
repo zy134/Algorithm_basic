99c07f4590d228455c5ad4a81160464bd707bfdd
zy134 <1257987991@qq.com>
2019-01-22 22:39:13 +0800

    1

---
+++ 3：高级设计与分析技术/分治算法/最大子数组-分治解/最大子数组-分治.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<Windows.h>
+using namespace std;
+constexpr int UNLIMITED=-999999;
+class  ArrayMsg{
+    ArrayMsg(int x,int y,int z):sum(x),left(y),right(z){}
+	ArrayMsg():sum(0),left(0),right(0){}
+    public:
+	int sum;
+	int left;
+	int right;
+};
+ArrayMsg Find_Cross_Max(int A[], ArrayMsg m)
+{
+    
+}
+int main()
+{
+    
+	system("pause");
+	return 0;
+}
