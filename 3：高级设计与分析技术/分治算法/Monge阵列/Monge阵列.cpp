fb8ab02002631b0d0483e7aabe6a7272786e9a29
zy134 <1257987991@qq.com>
2019-01-29 00:34:01 +0800

    q

--- 3：高级设计与分析技术/分治算法/Monge阵列/Monge阵列.cpp
+++ 3：高级设计与分析技术/分治算法/Monge阵列/Monge阵列.cpp
@@ -12,26 +12,103 @@ Monge矩阵的另一性质：
 #include<iostream>
 #include<Windows.h>
 using namespace std;
-//伪码
-void Find_Min_Left(int **A,int m,int n, int pos[], int row[])
+//伪码 
 /*
-pos[i]表示第i行最左最小元素的位置
-row[]用以表示子矩阵A'
+Find_Min_Left(A,pos)
+    A'=Devide_Monge(A)
+	Find_Min_Left(A',pos)
+	for(i=1 to m)
+	    for(j=pos[i-1] to pos[i+1])
+		    pos[i]=the min pos j for A[i][j]
 */
+void calculate(double **A, int r1, int r2, int min, int max, int *f)	//计算f(r1)到f(r2)
+
 {
-	for (int i = 0; i != m; i = 2 * i) {
-		row[i]=true;
-	}
-	Find_Min_Left(A,m,n,pos,row);
-	for (int j = 0; j != m; j++) {
-		if (row[j] == true) {
-			for (int temp=0,int k = pos[j - 1], int end = pos[j + 1]; k != end; k++) {
-				if (A[j][k] <= temp) {
-					temp=A[j][k];
-					pos[j]=k;
-				}
-			}
+
+	if (r1 > r2)return;
+
+	int r = (r1 + r2) / 2;
+
+	int result = min;
+
+	int flag = A[r][min];
+
+	for (int i = min + 1; i <= max; i++)	//寻找最左最小元素flag，和它的的下标result
+
+	{
+
+		if (A[r][i] < flag)
+
+		{
+
+			flag = A[r][i];
+
+			result = i;
+
 		}
+
 	}
+
+	f[r] = result;
+
+	calculate(A, r1, r - 1, min, result, f);
+
+	calculate(A, r + 1, r2, result, max, f);
+
+}
+
+
+
+bool isMonge(double **A, int m, int n)	//判断是否是Monge矩阵
+
+{
+
+	for (int i = 0; i < m - 1; i++)for (int j = 0; j < n - 1; j++)if (A[i][j] + A[i + 1][j + 1] > A[i + 1][j] + A[i][j + 1])return false;
+
+	return true;
+
+}
+
+int main()
+
+{
+
+	int m, n;
+
+	while (cin >> m >> n && m > 1 && n > 1)
+
+	{
+
+		double **A = new double*[m];		//Monge矩阵
+
+		int *f = new int[m];	//不需要在主函数里面进行初始化，这个工作由calculate函数完成
+
+		for (int i = 0; i < m; i++)
+
+		{
+
+			A[i] = new double[n];
+
+			for (int j = 0; j < n; j++)cin >> A[i][j];
+
+		}
+
+		if (isMonge(A, m, n))
+
+		{
+
+			cout << "这个是Monge矩阵" << endl;
+
+			calculate(A, 0, m - 1, 0, n - 1, f);
+
+			for (int i = 0; i < m; i++)cout << "第" << i << "行的最左最小元素的列下标是" << f[i] << endl;
+
+		}
+
+		else cout << "这个不是Monge矩阵";
+
+	}
+
+	return 0;
 
 }
